8361019b5946d987210ed0fb07012a191c287a54
Florian KOLBL <florian.kolbl@gmail.com>
2026-06-08 10:46:51 +0200

    Add CI/CD, PR template, and version bump workflow

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,8 @@ sys.path.insert(0, os.path.abspath("../src"))
 
 project = "neuromass"
 author = "neuromass contributors"
+release = "0.1.0"
+version = release
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
